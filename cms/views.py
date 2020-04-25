f24a238ad74a93fb783ab868a883d44ee5b40c12
meri <mdiaz>
2020-04-25 23:57:19 +0200

    añadiendo funcionalidad para manejar css

--- cms/views.py
+++ cms/views.py
@@ -4,22 +4,30 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Contenido
 
+
+def index(request):
+    content_list = Contenido.objects.all()
+    context = {'content_list': content_list}
+    return render(request, 'cms/index.html', context)
+
 @csrf_exempt
-def get_content(request, llave):
+def get_content(request, nombre):
     if request.method == "PUT":
         valor = request.body.decode('utf-8')
         try:
             #si ya existe esa llave la sustituyo por el nuevo valor
-            recurso = Contenido.objects.get(clave=llave)
+            recurso = Contenido.objects.get(clave=nombre)
             recurso.valor = valor
         except Contenido.DoesNotExist:
-            recurso = Contenido(clave=llave, valor=valor)
+            recurso = Contenido(clave=nombre, valor=valor)
         recurso.save()
 
-    elif request.method == "GET" or request.method == 'PUT':
-        respuesta = get_object_or_404(Contenido, clave=llave)
-        if name.endswith('.css'):
+    if request.method == "GET" or request.method == 'PUT':
+        recurso = get_object_or_404(Contenido, clave=nombre)
+        if nombre.endswith('.css'):
             content_type='text/css'
         else:
             content_type='text/html'
-        return HttpResponse(respuesta.valor, content_type=content_type)
+
+        return render(request, 'cms/content.html', {'recurso': recurso})
+        #return HttpResponse(recurso.valor, content_type=content_type)
