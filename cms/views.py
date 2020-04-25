a54992354c3c4fa85aac72ddddc891a9e7b0b63d
meri <mdiaz>
2020-04-25 23:37:42 +0200

    terminada la gestion de las peticiones

--- cms/views.py
+++ cms/views.py
@@ -1,22 +1,25 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404
 
 from .models import Contenido
 
 @csrf_exempt
 def get_content(request, llave):
-
+    if request.method == "PUT":
         valor = request.body.decode('utf-8')
         try:
             #si ya existe esa llave la sustituyo por el nuevo valor
-            respuesta = Contenido.objects.get(clave=llave)
-            respuesta.valor = valor
-            respuesta.save()
+            recurso = Contenido.objects.get(clave=llave)
+            recurso.valor = valor
         except Contenido.DoesNotExist:
-            if request.method == "PUT":
-                c = Contenido(clave=llave, valor=valor)
-                c.save()
-                return HttpResponse('<h1>Valor añadidio con exito</h1>')
-            elif request.method == "GET":
-                respuesta = "No existe contenido para la clave: " +llave
-                return HttpResponse(respuesta.valor)
+            recurso = Contenido(clave=llave, valor=valor)
+        recurso.save()
+
+    elif request.method == "GET" or request.method == 'PUT':
+        respuesta = get_object_or_404(Contenido, clave=llave)
+        if name.endswith('.css'):
+            content_type='text/css'
+        else:
+            content_type='text/html'
+        return HttpResponse(respuesta.valor, content_type=content_type)
