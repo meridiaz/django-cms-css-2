45e9cce72b9faf3b5729be400a8755690658e860
meri <mdiaz>
2020-04-25 23:17:16 +0200

    haciendo el get para css

--- cms/views.py
+++ cms/views.py
@@ -17,10 +17,25 @@ def get_content(request, llave):
             c = Contenido(clave=llave, valor=valor)
             c.save()
         return HttpResponse('<h1>Valor añadidio con exito</h1>')
-        
+
     elif request.method == "GET":
         try:
             respuesta = Contenido.objects.get(clave=llave).valor
         except Contenido.DoesNotExist:
             respuesta = "No existe contenido para la clave: " +llave
         return HttpResponse(respuesta)
+
+@csrf_exempt
+def get_content_css(request, recurso):
+    #if request.method == "PUT":
+        cuerpo = request.body.decode('utf-8')
+        try:
+            #si ya existe esa llave la sustituyo por el nuevo valor
+            respuesta = Contenido.objects.get(clave=llave)
+            respuesta.valor = cuerpo
+            respuesta.save()
+        except Contenido.DoesNotExist:
+            c = Contenido(clave=recurso, valor=cuerpo)
+            c.save()
+        return HttpResponse('<h1>Valor añadidio con exito</h1>')
+    #elif request.method == "GET":
